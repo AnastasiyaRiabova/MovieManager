df5ef53929b34650ab1f943f98750b569fac4472
Anastasiya Riabova <vereten.o@yandex.ru>
2023-04-12 21:17:04 +0400

    add test

--- src/test/java/MovieManagerTest.java
+++ src/test/java/MovieManagerTest.java
@@ -95,7 +95,7 @@ public class MovieManagerTest {
 
     @Test
     public void findLastTest3() {
-        MovieManager manager = new MovieManager(2);
+        MovieManager manager = new MovieManager(4);
         manager.add("Film 1");
         manager.add("Film 2");
         manager.add("Film 3");
@@ -103,7 +103,7 @@ public class MovieManagerTest {
         manager.add("Film 5");
         manager.add("Film 6");
 
-        String[] expected = {"Film 6", "Film 5"};
+        String[] expected = {"Film 6", "Film 5","Film 4","Film 3"};
         String[] actual = manager.findLast();
 
         Assertions.assertArrayEquals(expected, actual);
